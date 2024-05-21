c597edfc107273ccd8ecccde07781acf7fc14129
Ryan Alcoran <r.alcoran@gmail.com>
2024-05-21 10:30:18 -0400

    Add dialog modal to add a new book

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 const myLibrary = [];
+const book1 = new Book("Red Rising", "Pierce Brown", 382, true );
+const book2 = new Book("Golden Son", "Pierce Brown", 466, true );
+myLibrary.push(book1);
+myLibrary.push(book2);
+console.log(myLibrary);
+
+const booksContainer = document.querySelector(".books-container");
+
+const showButton = document.getElementById("showBookDialog");
+const addBookDialog = document.getElementById("addBookDialog");
+const submitButton = document.querySelector("#submitBookBtn");
+
+// "NEW BOOK" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  addBookDialog.showModal();
+});
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -8,6 +25,17 @@ function Book(title, author, pages, read) {
     this.info = () => `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
   }
 
-  function addBookToLibrary() {
-    // do stuff here
+  function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+  }
+
+  function displayLibary() {
+    myLibrary.forEach((book) => {
+      let bookCard = document.createElement("div");
+      bookCard.classList.add("book-card");
+      
+    });
+    
+
   }
